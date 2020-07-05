acb8cf9c021b8077712fc849c788b75b72d65f67
Olivier Martineau <omartineau8@gmail.com>
2020-07-05 16:49:43 -0400

    updating views

--- vue-client/src/layouts/Footer.vue
+++ vue-client/src/layouts/Footer.vue
@@ -1,7 +1,11 @@
 <template>
-  <footer class="footer is-dark">
+  <footer class="footer is-purple">
       <div class="content has-text-centered">
-          <p>This is a furry footer.</p>
+        <div class="columns">
+          <div class="column">About Us</div>
+          <div class="column">Contact Us</div>
+          <div class="column">Social Media</div>
+        </div>
       </div>
   </footer>
 </template>
